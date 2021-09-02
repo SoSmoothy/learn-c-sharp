59ae63891404a58dd80b79b435d50d522f98987d
Pham Anh Duc <88713288+SoSmoothy@users.noreply.github.com>
2021-09-02 18:43:32 +0700

    update

---
+++ learn-c-sharp/learn-exeption/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Data;
+
+namespace learn_exeption
+{
+    class DataToLong : Exception
+    {
+        public DataToLong() : base("Data to long")
+        {
+            
+        }
+    }
+
+    class Program
+    {
+        static void UserInput(string s)
+        {
+            if (s.Length > 5)
+            {
+                Exception e = new DataException();
+                throw e;
+            }
+        }
+        
+        static void Main(string[] args)
+        {
+            try
+            {
+                string s = Console.ReadLine();
+                UserInput(s);
+            }
+            catch (DataToLong ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ learn-c-sharp/learn-networking/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net.NetworkInformation;
+
+namespace learn_networking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ping ping = new Ping();
+            PingReply pingReply = ping.Send("google.com.vn");
+            if (pingReply?.Status == IPStatus.Success)
+            {
+                Console.WriteLine(pingReply.RoundtripTime + "ms");
+                Console.WriteLine(pingReply.Address);
+            }
+        }
+    }
+}
