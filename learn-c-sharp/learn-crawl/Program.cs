2d26265c43f7eec6e1671f0c081e21a930250a2d
Pham Anh Duc <88713288+SoSmoothy@users.noreply.github.com>
2021-09-12 23:35:42 +0700

    update

---
+++ learn-c-sharp/learn-crawl/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace learn_crawl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
--- learn-c-sharp/learn-event/Program.cs
+++ learn-c-sharp/learn-event/Program.cs
@@ -1,43 +1,27 @@
 ﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace learn_event
 {
-    class GotoUrlArgs : EventArgs
-    {
-        private string _url;
 
-        public GotoUrlArgs(string url)
-        {
-            _url = url;
-        }
-
-        public string Url
-        {
-            get { return _url; }
-        }
-    }
-
-    class User
-    {
-        public event EventHandler<GotoUrlArgs> ChangeUrlEvent; 
-        public void ChangeUrl(string url)
-        {
-            ChangeUrlEvent?.Invoke(this, new GotoUrlArgs(url));
-        }
-    }
-    
     class Program
     {
         static void Main(string[] args)
         {
-            User user = new User();
-            user.ChangeUrlEvent += (sender, urlArgs) =>
+            try
             {
-                Console.WriteLine($"Change URL to {urlArgs.Url}");
-            };
-            
-            user.ChangeUrl("google.com");
-            user.ChangeUrl("bing.com");
+                using (WebClient client = new WebClient())
+                {
+                    client.DownloadFile(new Uri("http://st.imageinstant.net/data/comics/32/vo-luyen-dinh-phong.jpg"), "asfaf.jpg");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
         }
     }
 }
